dc077ef415e4168d361ea4617553f963520450b6
mrchamsdev <developer@mrchams.com>
2026-02-18 10:34:04 +0530

    chore: added post and 3 feilds (#29)
    Co-authored-by: interns-mrchams <interns@mrchams.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
+// import "@/styles/globals.css";
+// import Head from "next/head";
+
+// export default function App({ Component, pageProps }) {
+//   <Head>
+//     <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+//     <meta name="robots" content={"noindex,nofollow"}/>
+//     <meta name="language" content="en" />
+
+//   </Head>
+//   return <Component {...pageProps} />;
+// }
 import "@/styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  <Head>
-    <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
-    <meta name="robots" content={"noindex,nofollow"}/>
-    <meta name="language" content="en" />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="language" content="en" />
+        <title>Zaanvar | Pet Business Portal</title>
+      </Head>
 
-  </Head>
-  return <Component {...pageProps} />;
+    
+      <Script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgYCTEby06dsd0hwEgMlijh4kBfbYeYTo&libraries=places"
+        strategy="beforeInteractive"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
